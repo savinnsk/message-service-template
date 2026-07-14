f11f49f5db70048a03f1fc61b404e4d86f76991c
savinnsk <saviodocuments@gmail.com>
2026-07-14 17:50:25 -0300

    docs: add docs with scalar

--- src/Application/Program.cs
+++ src/Application/Program.cs
@@ -1,12 +1,22 @@
+using Application.Configs;
 using message_service.Infra;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddInfraServices(builder.Configuration);
+builder.Services.AddDocumentation();
 
 var app = builder.Build();
 
+//if (app.Environment.IsDevelopment())
+//{
+       app.UseDocumentation();
+
+//}
+
+
+app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
